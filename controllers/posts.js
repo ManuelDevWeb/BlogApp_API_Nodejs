f1b724bb625323612f2226c161627b9a7e4f7842
ManuelDev <manueldeveloper17@gmail.com>
2023-02-03 00:42:52 -0500

    Add state to create new posts

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,5 +1,9 @@
+import jwt from "jsonwebtoken";
+
 // Config DB
 import { connectionDB as db } from "../db/index.js";
+// Environments
+import { environments } from "../config.js";
 // Respuestas personalizadas
 import { success, error } from "../utils/response.js";
 
@@ -50,7 +54,40 @@ const getPost = (req, res) => {
 const addPost = (req, res) => {};
 
 // Funcion para eliminar un post
-const deletePost = (req, res) => {};
+const deletePost = (req, res) => {
+  // Obteniendo y validando el token de la cookie
+  const token = req.cookies.access_token;
+
+  if (!token) {
+    return res.status(401).json("Not authenticated!");
+  }
+
+  // Verificamos token
+  jwt.verify(token, environments.jwt.secretKey, (err, userInfo) => {
+    if (err) {
+      return error(req, res, "Token is not valid", 403);
+    }
+
+    const postId = req.params.id;
+
+    // Query para eliminar el post
+    const query = "DELETE FROM posts WHERE `id`= ? AND `uid`= ?";
+
+    db.query(
+      query,
+      // Cada elemento equivale a un ?
+      [postId, userInfo.id],
+      (err, data) => {
+        if (err) {
+          // Retornamos respuesta personalizada
+          return error(req, res, "You can delete only your posts", 500);
+        }
+
+        return success(req, res, "Post has been deleted!", 200);
+      }
+    );
+  });
+};
 
 // Funcion para actualizar un post
 const updatePost = (req, res) => {};
